8d07fa5a965d1619d16ce52f2dc209758a29f3d7
ali.rezaiyan <ali.rezaiyan@bitpanda.com>
2025-07-05 10:08:10 +0200

    Update Gradle configuration: enable experimental JS canvas support and ensure AndroidX usage

--- monitoring-app/src/jsMain/kotlin/main.kt
+++
@@ -1,8 +0,0 @@
-import androidx.compose.runtime.Composable
-import androidx.compose.web.renderComposable
-
-fun main() {
-    renderComposable(rootElementId = "root") {
-        MonitoringDashboard()
-    }
-} 
